67ac8d90010749589a08800bb8db8185c97991d7
Antonio Silveira Guanaes Gomes <antonio@Antonios-MBP.lan1>
2019-07-29 01:54:12 -0400

    lksdfjkjadlkfj

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+//sets up the reusable Footer component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => ( <
+    header className = "footer" >
+    <
+    p > Antonio 's Game</p> <
+    /header>
+);
+
+export default Footer;
